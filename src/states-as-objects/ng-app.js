7c9f1fc07f10d1937b919ac31dba7c19dddfd341
Mat Janson Blanchet <mat.jb@jansensan.net>
2013-11-19 16:37:25 -0500

    Moved files to example-specific directory.

--- src/states-as-objects/ng-app.js
+++ src/states-as-objects/ng-app.js
@@ -1,4 +1,4 @@
-angular.module('page', ['ui.router'])
+angular.module('statesAsObjectsPage', ['ui.router'])
 
 .config(function ($stateProvider) {
 
@@ -7,109 +7,109 @@ angular.module('page', ['ui.router'])
   // parents
   var parent01 = {
     name: 'parent01',
-    templateUrl: 'static/templates/parent-01.html'
+    templateUrl: 'templates/parent-01.html'
   };
   var parent02 = {
     name: 'parent02',
-    templateUrl: 'static/templates/parent-02.html'
+    templateUrl: 'templates/parent-02.html'
   };
   var parent03 = {
     name: 'parent03',
-    templateUrl: 'static/templates/parent-03.html'
+    templateUrl: 'templates/parent-03.html'
   };
 
   // children
   var child01 = {
     name: 'child01',
     parent: parent01,
-    templateUrl: 'static/templates/child-01.html'
+    templateUrl: 'templates/child-01.html'
   };
   var child02 = {
     name: 'child02',
     parent: parent01,
-    templateUrl: 'static/templates/child-02.html'
+    templateUrl: 'templates/child-02.html'
   };
   var child03 = {
     name: 'child03',
     parent: parent02,
-    templateUrl: 'static/templates/child-03.html'
+    templateUrl: 'templates/child-03.html'
   };
   var child04 = {
     name: 'child04',
     parent: parent02,
-    templateUrl: 'static/templates/child-04.html'
+    templateUrl: 'templates/child-04.html'
   };
   var child05 = {
     name: 'child05',
     parent: parent03,
-    templateUrl: 'static/templates/child-05.html'
+    templateUrl: 'templates/child-05.html'
   };
   var child06 = {
     name: 'child06',
     parent: parent03,
-    templateUrl: 'static/templates/child-06.html'
+    templateUrl: 'templates/child-06.html'
   };
 
   // grandchilden
   var grandchild01 = {
     name: 'grandchild01',
     parent: child01,
-    templateUrl: 'static/templates/grandchild-01.html'
+    templateUrl: 'templates/grandchild-01.html'
   };
   var grandchild02 = {
     name: 'grandchild02',
     parent: child01,
-    templateUrl: 'static/templates/grandchild-02.html'
+    templateUrl: 'templates/grandchild-02.html'
   };
   var grandchild03 = {
     name: 'grandchild03',
     parent: child02,
-    templateUrl: 'static/templates/grandchild-03.html'
+    templateUrl: 'templates/grandchild-03.html'
   };
   var grandchild04 = {
     name: 'grandchild04',
     parent: child02,
-    templateUrl: 'static/templates/grandchild-04.html'
+    templateUrl: 'templates/grandchild-04.html'
   };
   var grandchild05 = {
     name: 'grandchild05',
     parent: child03,
-    templateUrl: 'static/templates/grandchild-05.html'
+    templateUrl: 'templates/grandchild-05.html'
   };
   var grandchild06 = {
     name: 'grandchild06',
     parent: child03,
-    templateUrl: 'static/templates/grandchild-06.html'
+    templateUrl: 'templates/grandchild-06.html'
   };
   var grandchild07 = {
     name: 'grandchild07',
     parent: child04,
-    templateUrl: 'static/templates/grandchild-07.html'
+    templateUrl: 'templates/grandchild-07.html'
   };
   var grandchild08 = {
     name: 'grandchild08',
     parent: child04,
-    templateUrl: 'static/templates/grandchild-08.html'
+    templateUrl: 'templates/grandchild-08.html'
   };
   var grandchild09 = {
     name: 'grandchild09',
     parent: child05,
-    templateUrl: 'static/templates/grandchild-09.html'
+    templateUrl: 'templates/grandchild-09.html'
   };
   var grandchild10 = {
     name: 'grandchild10',
     parent: child05,
-    templateUrl: 'static/templates/grandchild-10.html'
+    templateUrl: 'templates/grandchild-10.html'
   };
   var grandchild11 = {
     name: 'grandchild11',
     parent: child06,
-    templateUrl: 'static/templates/grandchild-11.html'
+    templateUrl: 'templates/grandchild-11.html'
   };
   var grandchild12 = {
     name: 'grandchild12',
     parent: child06,
-    templateUrl: 'static/templates/grandchild-12.html'
+    templateUrl: 'templates/grandchild-12.html'
   };
 
   // set state provider
