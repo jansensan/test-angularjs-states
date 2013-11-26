2b816e735da288c2663c69815048614d57c15223
Mat Janson Blanchet <mat.jb@jansensan.net>
2013-11-26 17:25:02 -0500

    Added state changes to test further.

--- src/views-and-nested-states/ng-app.js
+++ src/views-and-nested-states/ng-app.js
@@ -12,12 +12,37 @@ angular.module('app', ['stateManager'])
           templateUrl: "views/modal.html"
         }
       }
+    })
+
+    .state('page01', {
+      views: {
+        '': {
+          templateUrl: "views/page-01.html",
+        }
+      }
+    })
+
+    .state('modal', {
+      views: {
+        'modal': {
+          templateUrl: "views/modal.html",
+        }
+      }
     });
+})
+
+
+.controller('modalController', function ($scope, stateManager) {
+  $scope.closeModal = function () {
+    stateManager.setState('page01');
+  };
 })
 
 
 .controller('page01Controller', function ($scope, stateManager) {
   console.log('--- page01Controller ---');
+
+  stateManager.setState('page01');
 
   $scope.openModal = function () {
     stateManager.setState('modal');
